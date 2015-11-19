2af88ba31088cf79f996a5a9c9099f23c83454b7
= <=>
2015-11-18 19:30:37 -0600

    Fix for Local exception async tests.

--- test/TwentyTwenty.Storage.Local.Test/ExceptionTestsAsync.cs
+++ test/TwentyTwenty.Storage.Local.Test/ExceptionTestsAsync.cs
@@ -9,42 +9,41 @@ namespace TwentyTwenty.Storage.Local.Test
             : base(fixture) { }
 
         [Fact]
-        public void Test_Blob_Deleted_Directory_Exception_Async()
+        public async void Test_Blob_Deleted_Directory_Exception_Async()
         {
-            var ex = Assert.ThrowsAsync<StorageException>(async () =>
+            var ex = await Assert.ThrowsAsync<StorageException>(() =>
             {
-                await _provider.DeleteBlobAsync("asdf", "asdf.txt");
+                return _provider.DeleteBlobAsync("asdf", "asdf.txt");
             });
 
-            Assert.Equal(ex.Result.ErrorCode, 1005);
+            Assert.Equal(ex.ErrorCode, 1005);
         }
 
         [Fact]
-        public void Test_Get_Blob_Stream_FileNotFound_Exception_Async()
+        public async void Test_Get_Blob_Stream_FileNotFound_Exception_Async()
         {
-            var ex = Assert.ThrowsAsync<StorageException>(async () =>
+            var container = GetRandomContainerName();
+            var blobName = GenerateRandomName();
+            var data = GenerateRandomBlobStream();
+            CreateNewFile(container, blobName, data);
+
+            var ex = await Assert.ThrowsAsync<StorageException>(() =>
             {
-                var container = GetRandomContainerName();
-                var blobName = GenerateRandomName();
-                var data = GenerateRandomBlobStream();
-
-                CreateNewFile(container, blobName, data);
-
-                await _provider.GetBlobStreamAsync(container, "asdf.txt");
+                return _provider.GetBlobStreamAsync(container, "asdf.txt");
             });
 
-            Assert.Equal(ex.Result.ErrorCode, 1004);
+            Assert.Equal(ex.ErrorCode, 1004);
         }
 
         [Fact]
-        public void Test_Get_Blob_Stream_Directory_Exception_Async()
+        public async void Test_Get_Blob_Stream_Directory_Exception_Async()
         {
-            var ex = Assert.ThrowsAsync<StorageException>(async () =>
+            var ex = await Assert.ThrowsAsync<StorageException>(() =>
             {
-                await _provider.GetBlobStreamAsync("asdf", "asdf.txt");
+                return _provider.GetBlobStreamAsync("asdf", "asdf.txt");
             });
 
-            Assert.Equal(ex.Result.ErrorCode, 1005);
+            Assert.Equal(ex.ErrorCode, 1005);
         }
     }
 }
